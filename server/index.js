f4900f7b757c4176d78e6a53cefba2e99eb16569
jaideep2121 <sachdevajaideep3@gmail.com>
2024-11-07 17:38:59 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://courseapp-eight.vercel.app",
 		credentials:true,
 	})
 )
